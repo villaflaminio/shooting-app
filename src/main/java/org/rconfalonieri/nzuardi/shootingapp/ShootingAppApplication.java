5a09f60055caea18c4bc92bd95f6d6b790b2300b
andreaperotti <90187741+andreaperotti@users.noreply.github.com>
2022-09-20 15:56:57 +0200

    Init project

---
+++ src/main/java/org/rconfalonieri/nzuardi/shootingapp/ShootingAppApplication.java
@@ -0,0 +1,13 @@
+package org.rconfalonieri.nzuardi.shootingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShootingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShootingAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/rconfalonieri/nzuardi/shootingapp/ShootingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rconfalonieri.nzuardi.shootingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShootingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
